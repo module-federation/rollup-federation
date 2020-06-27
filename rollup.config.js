4b0f6400070ea1ad0b2ae3d3b58d8181155a42f7
Marais Rossouw <me@maraisr.com>
2020-06-27 17:03:52 +1000

    chore: Small repo cleanups

--- rollup.config.js
+++ rollup.config.js
@@ -3,11 +3,11 @@ import resolve from '@rollup/plugin-node-resolve';
 import pkg from './package.json';
 
 export default {
-  input: 'src/index.js',
-  plugins: [resolve()],
-  external: ['path', '@rollup/plugin-inject'],
-  output: [
-    { format: 'cjs', file: pkg.main },
-    { format: 'esm', file: pkg.module }
-  ]
+	input: 'src/index.js',
+	plugins: [resolve()],
+	external: ['path', '@rollup/plugin-inject'],
+	output: [
+		{ format: 'cjs', file: pkg.main },
+		{ format: 'esm', file: pkg.module },
+	],
 };
